80d50a215e1b5d2c1268a77ef67add15cde8f752
Pate Katechis <patekatechis@Pates-MacBook-Pro.local>
2017-11-30 11:18:16 -0600

    First commit as Gitters!

--- Digital Programs App/ViewController.swift
+++ Digital Programs App/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        print(buttonCount)
+        
+        if buttonCount >= 5 {
+            view.backgroundColor = UIColor.yellow
+            myLabel.text = "almost...."
+            
+        }
+        if buttonCount >= 10 {
+        view.backgroundColor = UIColor.red
+            myLabel.text = "WOOOO!"    }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
